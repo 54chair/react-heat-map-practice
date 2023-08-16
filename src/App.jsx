19fbfb79862756924b428d5dfadb688b64597f7a
54chair <takukoshi3721@gmail.com>
2023-08-16 10:34:38 +0900

    githubライクなheatmap

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import HeatMap from '@uiw/react-heat-map';
 
-function App() {
-  const [count, setCount] = useState(0)
+const value = [
+  { date: '2016/01/11', count:2 },
+  ...[...Array(17)].map((_, idx) => ({ date: `2016/01/${idx + 10}`, count: idx })),
+  ...[...Array(17)].map((_, idx) => ({ date: `2016/02/${idx + 10}`, count: idx })),
+  { date: '2016/04/12', count:2 },
+  { date: '2016/05/01', count:5 },
+  { date: '2016/05/02', count:5 },
+  { date: '2016/05/03', count:1 },
+  { date: '2016/05/04', count:11 },
+  { date: '2016/05/08', count:32 },
+];
 
+const Demo = () => {
+  const [size, setSize] = useState(0)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <label style={{ userSelect: 'none' }}>
+        <input
+          type="checkbox"
+          checked={size === 0}
+          onChange={(e) => setSize(e.target.checked ? 0 : 12)}
+        />
+        {size === 0 ? ' Hide' : ' Show'} Legend
+      </label>
+      <HeatMap
+        width={600}
+        value={value}
+        legendCellSize={size}
+        startDate={new Date('2016/01/01')}
+      />
+    </div>
   )
-}
-
-export default App
+};
+export default Demo
